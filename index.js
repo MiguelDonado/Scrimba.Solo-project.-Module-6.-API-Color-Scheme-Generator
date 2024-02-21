91c309b828b4cd46869ebfcb2b0ff92eae6b7c4d
MiguelDonado <miguel.5799@hotmail.es>
2024-02-21 20:33:24 +0100

    Refactor index.js and finish project.

--- index.js
+++ index.js
@@ -7,34 +7,43 @@ document.addEventListener('submit', function(e){
     e.preventDefault()
     cleanPreviousColorScheme()
     const [ color, mode ] = getArgumentsColorAndMode()
-    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&format=json&count=5&w=1300`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&format=json&count=5`)
         .then(response => response.json())
         .then(data => {
             renderHexCodes(data.colors)
         })
     }
-    )
+)
+    
+function cleanPreviousColorScheme() {
+    hexColorsEl.innerHTML = ``
+    colorsContainerEl.innerHTML = ``    
+}
 
 function getArgumentsColorAndMode(){
     color = seedColorEl.value.replace("#","")
     mode = colorSchemeEl.value.toLowerCase()
     return [color,mode]
 }
-
+    
 function renderHexCodes(colorsObject) {
     colorsObject.forEach(color => {
-        hexColor = color.hex.value
-        hexColorsEl.innerHTML+=`<h3>${hexColor}</h3>`
-        let colorEl = document.createElement("div")
-        colorEl.style.backgroundColor = hexColor
-        colorsContainerEl.appendChild(colorEl)
+        getHexColorAndPopulateBgText(color)
     }
-        )
+)}
+
+function getHexColorAndPopulateBgText(colorObject) {
+    hexColor = colorObject.hex.value
+    hexColorsEl.innerHTML+=`<h3>${hexColor}</h3>`
+    createContainerBackground(hexColor)
 }
 
-function cleanPreviousColorScheme() {
-    hexColorsEl.innerHTML = ``
-    colorsContainerEl.innerHTML = ``    
+function createContainerBackground(hexColor) {
+    let colorEl = document.createElement("div")
+    colorEl.style.backgroundColor = hexColor
+    colorsContainerEl.appendChild(colorEl)
 }
+
+
 
 
